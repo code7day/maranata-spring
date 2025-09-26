102df6839b21714abf838bfd5aa5a7fbeeec2ae9
Edu. Flores <zedu77@gmail.com>
2025-09-26 15:29:34 -0500

    fix

--- app/Livewire/SurveyMaranata.php
+++ app/Livewire/SurveyMaranata.php
@@ -40,7 +40,7 @@ class SurveyMaranata extends Component
 
     public function mount()
     {
-        $this->deadlineIsoString = Carbon::today('America/Lima')->setTime(16, 0, 0)->toIso8601String();
+        $this->deadlineIsoString = Carbon::today('America/Lima')->setTime(17, 00, 0)->toIso8601String();
 
         if (Session::get('report_unlocked')) {
             $this->isReportUnlocked = true;
